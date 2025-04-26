cf502728f244b9311b08d198f9783f7d00eb6f77
saikumarmandava2 <saikumarmandava2@gmail.com>
2025-04-26 15:29:04 -0500

    test

---
+++ test/src/com/java/test/testinggit.java
@@ -0,0 +1,12 @@
+package com.java.test;
+
+public class testinggit {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Test is successful");
+		// TODO Auto-generated method stub
+
+	}
+
+}
